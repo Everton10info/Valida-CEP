3891249431a7d8734c0f92bc1603cb61d3f3cbc1
everton10info <everton10info@gmail.com>
2022-09-08 02:17:31 -0300

    Removido campo não usado do model

--- lib/models/model_cep.dart
+++ lib/models/model_cep.dart
@@ -1,5 +1,4 @@
 class CepData {
-  int? id;
   int status;
   String code;
   String state;
@@ -7,35 +6,31 @@ class CepData {
   String district;
   String address;
 
-  CepData({ 
+  CepData({
     required this.status,
     required this.code,
     required this.state,
     required this.city,
     required this.district,
     required this.address,
- 
   });
 
   Map<String, dynamic> toMap() => {
         "status": status,
-        "code": code.replaceAll('-',''),
+        "code": code.replaceAll('-', ''),
         "state": state,
         "city": city,
         "district": district,
         "address": address
-   
       };
 
   factory CepData.fromjson(Map<String, dynamic> map) {
     return CepData(
-      status: map['status'],
-      code: map['code'].replaceAll('-','') as String,
-      state: map['state'],
-      city: map['city'] ?? 'Não encontrado',
-      district: map['district'] ?? 'Não encontrado',
-      address: map['address']?? 'Não encontrado'
-    
-    );
+        status: map['status'],
+        code: map['code'].replaceAll('-', '') as String,
+        state: map['state'],
+        city: map['city'] ?? 'Não encontrado',
+        district: map['district'] ?? 'Não encontrado',
+        address: map['address'] ?? 'Não encontrado');
   } // caso CEP seja valido e api mandar campos vazios aparecerá como não encontrado
 }
